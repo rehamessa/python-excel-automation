585a0b059c3a0203a28d030b14c913e5eedfd42a
reham.essa <rehamessa96@gmail.com>
2026-09-13 21:42:42 +0300

    calculate the aggregation columns

--- python-excel-automation.py
+++ python-excel-automation.py
@@ -9,8 +9,21 @@ sheet=workbook.active
 #-------------------------------------------------------
 #Read all sales value
 #-------------------------------------------------------
+highest_sales=0
+total_sales=0
 for row in range(2,sheet.max_row+1):
     sales=sheet[f"B{row}" ] .value
-    print(sales)
+
+    total_sales+=sales# total sales
+
+    if sales>highest_sales:
+        highest_sales=sales #highest sales
+
+average_sale=total_sales/(sheet.max_row-1) #average sales
+
+product_count=sheet.max_row-1 #calculate product count
+
+
+
 
     
